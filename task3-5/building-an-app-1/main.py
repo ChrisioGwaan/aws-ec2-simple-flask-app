33c4ea66fabdbcbc24f7d8d676da84298b81ce9b
Chrisio Gwaan <chris322322@gmail.com>
2023-04-07 00:51:35 +1000

    Fix bugs and make improvements

--- task3-5/building-an-app-1/main.py
+++ task3-5/building-an-app-1/main.py
@@ -16,6 +16,8 @@
 # [START gae_python3_render_template]
 import datetime
 
+import os
+
 from flask import Flask, render_template, request, flash, redirect, url_for
 
 import key_configer as keys
@@ -28,6 +30,7 @@ from botocore.exceptions import ClientError
 
 app = Flask(__name__)
 
+# Configure the AWS credentials
 dynamodb = boto3.resource('dynamodb',
                     aws_access_key_id=keys.ACCESS_KEY_ID,
                     aws_secret_access_key=keys.ACCESS_SECRET_KEY,
@@ -60,6 +63,8 @@ def signup():
             }
         )
         
+        # if the email already exists, return a message
+        # else, add the new user to the database
         if 'Item' in response:
             msg = "The email already exists"
             return render_template('signup.html', name = msg)
@@ -76,7 +81,7 @@ def signup():
     return render_template('signup.html')
 
 @app.route('/login')
-def login():    
+def login():
     return render_template('login.html')
 
 @app.route('/check_login', methods = ['GET','POST'])
@@ -87,13 +92,16 @@ def check_login():
         password = request.form['password']
         
         table = dynamodb.Table('login')
+        
         response = table.query(
-            KeyConditionExpression=Key('email').eq(email)
+            KeyConditionExpression=Key('email').eq(email),
+            FilterExpression=Attr('password').eq(password)
         )
         items = response['Items']
-        # print(items[0]['password'])
 
-        if password == items[0]['password']:
+        # if the email and password match, redirect to the main page
+        # else, return a message
+        if items:
             return redirect(url_for('main_page', email=email))
         else:
             msg = "email or password is invalid"
@@ -104,14 +112,18 @@ def check_login():
 def logout():
     return render_template('login.html')
 
+# pass through the email to the main page
 @app.route('/main_page/<email>', methods = ['GET'])
 def main_page(email):
+    # query the database using email to get the user_name
     table = dynamodb.Table('login')
     response = table.query(KeyConditionExpression=Key('email').eq(email))
     items = response['Items']
     user_name = items[0]['user_name']
+    
     return render_template('main_page.html', user_name=user_name, email=email)
 
+# pass through the email on subscription_area that continued with the main page
 @app.route('/main_page/<email>/subscription_area', methods = ['GET'])
 def subscription_area(email):
     s3 = boto3.client('s3')
@@ -122,33 +134,52 @@ def subscription_area(email):
     items = response['Items']
     
     # if items list is empty, return a message
+    # else, get the url of the image from s3
     if not items:
         msg = "You have not subscribed to any stocks yet."
         return render_template('subscription_area.html', email=email, msg=msg)
     
     for item in items:
         
+        # remove the space in the artist name
         temp_artist = item['artist'].replace(' ', '')
         
+        # generate the same name as the image in s3
         key = f"{temp_artist}.jpg"
         
+        # get the url of the image
         url = s3.generate_presigned_url('get_object', Params={'Bucket': 's3830776-assignment1', 'Key': key})
         item['url'] = url
 
     return render_template('subscription_area.html', email=email, items=items)
 
+# pass through the email on query_area that continued with the main page
 @app.route('/main_page/<email>/query_area', methods = ['GET', 'POST'])
 def query_area(email):
     if request.method == 'POST':
         title = request.form['title']
+        
+        # if the year is not empty, convert it to int
+        # else, set it to None
         if request.form['year'] != "":
             year = int(request.form['year'])
         else:
             year = None
+            
         artist = request.form['artist']
         
         table = dynamodb.Table('music')
 
+        # the if-else statements are used to generate the query based on the input
+        # including all the combinations of title, year and artist
+        # <title, year, artist>
+        # <title, year>
+        # <title, artist>
+        # <year, artist>
+        # <title> only
+        # <year> only
+        # <artist> only
+        # ELSE - return all the items in the table
         if title and year and artist:
             response = table.query(
                 KeyConditionExpression=Key('title').eq(title) & Key('artist').eq(artist),
@@ -160,15 +191,11 @@ def query_area(email):
                 FilterExpression=Attr('year').eq(year)
             )
         elif title and artist:
-            response = table.query(
-                KeyConditionExpression=Key('title').eq(title) & Key('artist').eq(artist)
-            )
+            response = table.query(KeyConditionExpression=Key('title').eq(title) & Key('artist').eq(artist))
         elif year and artist:
             response = table.scan(FilterExpression=Attr('year').eq(year) & Attr('artist').eq(artist))
         elif title:
-            response = table.query(
-                KeyConditionExpression=Key('title').eq(title)
-            )
+            response = table.query(KeyConditionExpression=Key('title').eq(title))
         elif year:
             response = table.scan(FilterExpression=Attr('year').eq(year))
         elif artist:
@@ -183,6 +210,7 @@ def query_area(email):
             msg = "No result is retrieved. Please query again!"
             return render_template('query_area.html', email=email, msg=msg)
         
+        # get the url of the image from s3
         s3 = boto3.client('s3')
         for item in items:
             temp_artist = item['artist'].replace(' ', '')
@@ -195,6 +223,7 @@ def query_area(email):
         return render_template('query_area.html', email=email, items=items)
     return render_template('query_area.html', email=email)
 
+# Subscribe action
 @app.route('/main_page/<email>/query_area/subscribe', methods=['POST'])
 def subscribe(email):
     email = request.form['email']
@@ -202,12 +231,9 @@ def subscribe(email):
     artist = request.form['artist']
     year = request.form['year']
     
-    # Find the subscription in DynamoDB
     table = dynamodb.Table('subscription')
 
-    response = table.scan(
-        FilterExpression=Attr('email').eq(email) & Attr('title').eq(title) & Attr('artist').eq(artist) & Attr('year').eq(year)
-        )
+    response = table.scan(FilterExpression=Attr('email').eq(email) & Attr('title').eq(title) & Attr('artist').eq(artist) & Attr('year').eq(year))
     
     items = response['Items']
 
@@ -222,6 +248,8 @@ def subscribe(email):
 
     id = 0 # default id
 
+    # if the subscription table is empty, id starts from 0
+    # else, get the max id and add 1 to it
     if not items:
         table.put_item(
             Item={
@@ -252,9 +280,9 @@ def subscribe(email):
     
     return render_template('query_area.html', email=email, msg=msg)
 
+# Remove action
 @app.route('/main_page/<email>/subscription_area/remove', methods=['POST'])
 def remove(email):
-    email = request.form['email']
     title = request.form['title']
     artist = request.form['artist']
     year = request.form['year']
@@ -262,9 +290,7 @@ def remove(email):
     # Find the subscription in DynamoDB
     table = dynamodb.Table('subscription')
 
-    response = table.scan(
-        FilterExpression=Attr('email').eq(email) & Attr('title').eq(title) & Attr('artist').eq(artist) & Attr('year').eq(year)
-        )
+    response = table.scan(FilterExpression=Attr('email').eq(email) & Attr('title').eq(title) & Attr('artist').eq(artist) & Attr('year').eq(year))
     
     items = response['Items']
     id = items[0]['id']
@@ -288,6 +314,6 @@ if __name__ == '__main__':
     # the "static" directory. See:
     # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
     # App Engine itself will serve those files as configured in app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)), debug=True)
 # [END gae_python3_render_template]
 # [END gae_python38_render_template]
